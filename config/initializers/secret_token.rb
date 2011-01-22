a0c24ee2662999776ae562cfdd9647fd749f6415
Drew Chandler <andrewschandler@gmail.com>
2011-01-22 11:21:30 -0800

    Initial Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwissPairings::Application.config.secret_token = '746d77731f47a4ce1856844be9dab85834de29b789a14f3bd175e08b3163b9312d4680c33b34d40dabce860c373c4bd16d9118fb01caeac87a8e69ceee14193e'
