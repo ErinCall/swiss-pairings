bc2357349d39ebd206653dcef32d2340175a1de3
Drew Chandler <andrewschandler@gmail.com>
2011-01-29 11:42:05 -0800

    Add examples for start_round after the tournament has started

--- spec/controllers/tournaments_controller_spec.rb
+++ spec/controllers/tournaments_controller_spec.rb
@@ -1,10 +1,28 @@
 require 'spec_helper'
 
 describe TournamentsController do
+  shared_examples_for 'start_round' do
+    it 'should increment the current round' do
+      tournament.should_receive(:next_round)
+
+      post :start_round, id: 'foo'
+    end
+
+    it 'should generate the matches for the round' do
+      tournament.should_receive(:generate_matches)
+
+      post :start_round, id: 'foo'
+    end
+  end
+
   describe '#start_round' do
+    let(:tournament) { mock_model(Tournament, id: 'foo') }
+
     context 'when starting a tournament' do
-      let(:tournament) { mock_model(Tournament, id: 'foo', started?: false) }
-      before(:each) { Tournament.stub(:find).with('foo').and_return(tournament) }
+      before(:each) do
+        Tournament.stub(:find).with('foo').and_return(tournament)
+        tournament.stub(:started?).and_return(false)
+      end
 
       context 'and there are too few players' do
         before(:each) { tournament.stub_chain(:players, :count).and_return(0) }
@@ -31,23 +49,31 @@ describe TournamentsController do
           tournament.stub(:next_round)
         end
 
+        it_should_behave_like 'start_round'
+
         it 'should calculate the total number of rounds' do
           tournament.should_receive(:calculate_total_rounds)
 
           post :start_round, id: 'foo'
         end
+      end
+    end
 
-        it 'should increment the current round' do
-          tournament.should_receive(:next_round)
+    context 'after the tournament has been started' do
+      before(:each) do
+        Tournament.stub(:find).with('foo').and_return(tournament)
+        tournament.stub(:started?).and_return(true)
+        tournament.stub_chain(:players, :count).and_return(5)
+        tournament.stub(:generate_matches)
+        tournament.stub(:next_round)
+      end
 
-          post :start_round, id: 'foo'
-        end
+      it_should_behave_like 'start_round'
 
-        it 'should generate the matches for the round' do
-          tournament.should_receive(:generate_matches)
+      it 'should not calculate the total number of rounds' do
+        tournament.should_not_receive(:calculate_total_rounds)
 
-          post :start_round, id: 'foo'
-        end
+        post :start_round, id: 'foo'
       end
     end
   end
