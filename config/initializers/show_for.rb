3d72290ab36c6234ed9ea239b4e3e651c58599a3
Drew Chandler <andrewschandler@gmail.com>
2011-01-30 18:35:54 -0800

    First pass of styling

--- config/initializers/show_for.rb
+++ config/initializers/show_for.rb
@@ -4,19 +4,19 @@ ShowFor.setup do |config|
   # config.show_for_tag = :div
 
   # The tag which wraps each attribute/association call. Default is :p.
-  # config.wrapper_tag = :dl
+  config.wrapper_tag = :dl
 
   # The tag used to wrap each label. Default is :strong.
-  # config.label_tag = :dt
+  config.label_tag = :dt
 
   # The tag used to wrap each content (value). Default is nil.
-  # config.content_tag = :dd
+  config.content_tag = :dd
 
   # The DOM class set for blank content tags. Default is "blank".
   # config.blank_content_class = 'no_content'
 
   # The separator between label and content. Default is "<br />".
-  # config.separator = "<br />"
+  config.separator = nil
 
   # The tag used to wrap collections. Default is :ul.
   # config.collection_tag = :ul
