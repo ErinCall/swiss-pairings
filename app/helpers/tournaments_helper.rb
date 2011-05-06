aacd42bf9ccb8fc727fa146a6517f998a4061aff
Andrew Lorente <andrew.lorente@gmail.com>
2011-05-06 12:23:50 -0700

    Show standings for finished tournaments.

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -12,4 +12,21 @@ module TournamentsHelper
       nil
     end
   end
+
+  def tournament_status(tournament)
+    status = ''
+    if tournament.players.count > 0
+      status = pluralize(tournament.players.count, 'player')
+    end
+
+    if tournament.finished_rounds > 0
+      status << "; #{tournament.finished_rounds} of #{tournament.total_rounds} rounds complete"
+    end
+
+    return status
+  end
+
+  def tournament_results(tournament)
+    tournament.players.sort.reverse
+  end
 end
